a39ebb420ffba578610f446d8d6c143e58ba4f70
PLStenger <Pierrelouis.stenger@gmail.com>
2016-06-07 12:53:25 +0200

    It's a Final Countdown !

--- LnPD2.R
+++ LnPD2.R
@@ -10,4 +10,8 @@ dfrx <- data.frame(K = unique(dat$K),
 				sd = tapply(dat$LnPD, dat$K, sd))
 
 # Plot
+<<<<<<< Updated upstream
 gg4 <- qplot(dfrx$K, dfrx$mean) + scale_x_discrete(limit = c(dfrx$K)) + geom_errorbar(aes(x=dfrx$K, ymin=dfrx$mean-dfrx$sd, ymax=dfrx$mean+dfrx$sd), width=0.1) + geom_line(colour = "blue") + labs(x = "K", y = "LnPD") + theme(legend.position="none")
+=======
+gg4 <- qplot(dfrx$K, dfr$mean) + scale_x_discrete(limit = c(dfrx$K)) + geom_errorbar(aes(x=dfrx$K, ymin=dfrx$mean-dfrx$sd, ymax=dfrx$mean+dfr$sd), width=0.1) + geom_line(colour = "blue") + labs(x = "K", y = "LnPD") + theme(legend.position="none")
+>>>>>>> Stashed changes
